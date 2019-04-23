2e2bd070af0a185cc2a124da529d1e01abfb4602
silvicardo <silvicardo@gmail.com>
2019-04-23 10:23:52 +0200

    forceJsonResponse middleware

---
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ForceJsonResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //All routes returns a JSON
+        $request->headers->set('Accept','application/json');
+
+        return $next($request);
+    }
+}
